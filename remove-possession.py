30d1b4eff71a7ab9f8b9752cc903561c785aa7ab
Mooncoco <feilong.yu@yahoo.com>
2014-03-26 10:12:32 +0800

    Modify Source

--- remove-possession.py
+++ remove-possession.py
@@ -24,12 +24,13 @@ strlist = str(result).split('  TCP    ')
 
 for value in strlist:
     if value.find('LISTENING') > 0:
-    	value.strip().lstrip()
-    	commond_task_port = value[len(value)-10:len(value)-6]
-    	print(commond_task_port)
-    	commond_netstat_find += " \"" + commond_task_port + "\""
-    	print(commond_netstat_find)
-    	break
+        value.strip().lstrip()
+#         commond_task_port = value[len(value)-10:len(value)-6]
+        commond_task_port = value[len(value)-13:len(value)-9]
+        print("commond_task_port::" + commond_task_port)
+        commond_netstat_find += " \"" + commond_task_port + "\""
+        print("commond_netstat_find" + commond_netstat_find)
+        break
 
 res_task = subprocess.Popen(commond_netstat_find, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -40,11 +41,15 @@ str_res_list = str(reult_task)[3:10]
 print("str_res_list::" + str_res_list)
 
 str_stop_task = "\"" + commond_kill_task + " " + str_res_list + "\""
-
+ 
 print(str_stop_task)
 
-result_code = os.system(str_stop_task)
+os.system(str_stop_task)
 
-print(result_code)
-# if result_code == 0:
-# 	print("--------------")
+os.system("adb start-server")
+
+os.system("pause")
+
+# result_code = os.system(str_stop_task)
+ 
+# print(result_code)
